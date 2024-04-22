6e27400917668e22306540b0b791d78240048f6f
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-22 15:52:25 -0300

    declarando a rota de registro de usuario

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,11 @@
+import { Router} from "express";
+import { register } from "../controllers/AuthController"; 
+
+
+const router = Router()
+
+router.post('/register', register)
+
+
+export default router 
+
