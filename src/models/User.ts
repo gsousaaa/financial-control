fa62e3ccee2379fe2eb552d7771dbe3260fbe6f7
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-22 15:51:22 -0300

    Definindo o relacionamento entre as tabelas.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
-import { Model, DataTypes } from 'sequelize';
-
+import { Model, DataTypes, HasMany } from 'sequelize';
 import { sequelize } from '../instances/mysql';
+import { Movement } from './Movement';
 
 export interface UserInstance extends Model {
     id: number,
@@ -24,14 +24,16 @@ export const User = sequelize.define<UserInstance>('User', {
     }, 
     email: {
         allowNull: false,
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        unique: true
     },
     passwordHash: {
         allowNull: false,
         type: DataTypes.STRING
     }, 
     saldo: {
-        allowNull: false, 
+        allowNull: true, 
+        defaultValue: 0,
         type: DataTypes.FLOAT
     }
 
@@ -39,3 +41,17 @@ export const User = sequelize.define<UserInstance>('User', {
         tableName: 'users',
         timestamps: false
 })
+
+User.hasMany(Movement, ({foreignKey: 'user_id'}))
+Movement.belongsTo(User, ({foreignKey: 'user_id'}))
+
+
+sequelize.sync()
+    .then(() => {
+        
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+
