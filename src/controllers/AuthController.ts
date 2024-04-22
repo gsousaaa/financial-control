aea5116b38a2b6839adcb582b207d74e2aa1c17a
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-22 17:50:43 -0300

    Declarando e configurando o método para Login

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,12 +5,14 @@ import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
 import { isEmail } from 'validator';
 
-interface body { 
+dotenv.config()
+
+interface bodyLogin {
     email: string,
     password: string
 }
 
-interface bodyRegister extends body {
+interface bodyRegister extends bodyLogin {
     name: string
 }
 
@@ -40,11 +42,47 @@ export const register = async (req: Request, res: Response) => {
 
     let newUser = await User.create(newUserObj)
     let token = JWT.sign(
-        {id: newUser.id, email: newUser.email}, 
+        { id: newUser.id, email: newUser.email },
         process.env.JWT_SECRET_KEY as string,
-        {expiresIn: '2h'}
+        { expiresIn: '2h' }
     )
 
     return res.status(201).json({ id: newUser.id, email: newUser.email, saldo: newUser.saldo, token })
 }
 
+export const login = async (req: Request, res: Response) => {
+    let { email, password }: bodyLogin = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Preencha os campos!' })
+    }
+
+    if (!isEmail(email)) {
+        return res.status(400).json({ message: 'E-mail inválido' })
+    }
+
+    let user = await User.findOne({
+        where: {
+            email
+        }
+    })
+
+    if (!user) {
+        return res.status(400).json({ message: 'E-mail e/ou senha inválidos' })
+    }
+
+    let passwordMatch = bcrypt.compare(password, user.passwordHash)
+
+    if (!passwordMatch) {
+        return res.status(400).json({ message: 'E-mail e/ou senha inválidos' })
+    }
+
+    let token = JWT.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: '2h' }
+    )
+    
+    return res.status(200).json({ message: 'Login efetuado com sucesso!', token })
+
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router} from "express";
-import { register } from "../controllers/AuthController"; 
+import * as AuthController from "../controllers/AuthController"; 
 
 
 const router = Router()
 
-router.post('/register', register)
+router.post('/register', AuthController.register)
+router.post('/login', AuthController.login)
 
 
 export default router 
