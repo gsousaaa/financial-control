654bdb50b4dab2c8d97d0a9219f06cb3d899c332
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-24 11:11:04 -0300

    Corrigindo erro na rota DELETE, agora o id da movimentação é passado na url.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,7 +6,8 @@ const router = Router()
 
 router.post('/movement',  Auth.private, UserController.createMovement)
 router.put('/movement', Auth.private, UserController.updateMovement)
-router.delete('/movement', Auth.private, UserController.deleteMovement)
+router.delete('/movement/:id', Auth.private, UserController.deleteMovement)
+router.get('/movements', Auth.private, UserController.getMovements)
 
 
 
